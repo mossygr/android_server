575c68b1c59bae4906f3c1c0151ebf4869ddb806
mossygr <mossylp@gmail.com>
2018-07-17 09:17:40 +0300

    second

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
-from requests import get
 import http.server
 import socketserver
+from requests import get
 
 
-ip = get('https://api.ipify.org').text
 PORT = 8000
+ip = get('https://api.ipify.org').text
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    
+    print("Visit the following url: ", ip, ":", PORT,sep='')
     httpd.serve_forever()
-httpd.serve_forever()
 
-print ("serving at ",ip,":", PORT)
+
